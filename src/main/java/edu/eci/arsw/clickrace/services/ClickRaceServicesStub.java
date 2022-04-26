d27ce7ae4562696a495ca1784183395b28579247
DavidPZ666 <davidpz616@gmail.com>
2022-04-25 22:55:59 -0500

    Bono realizado XD

---
+++ src/main/java/edu/eci/arsw/clickrace/services/ClickRaceServicesStub.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arsw.clickrace.services;
+
+import edu.eci.arsw.clickrace.model.RaceParticipant;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentSkipListSet;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author hcadavid
+ */
+@Service
+public class ClickRaceServicesStub implements ClickRaceServices {
+
+    //racenum x racersid_set
+    ConcurrentHashMap<Integer, Set<RaceParticipant>> racesData=new ConcurrentHashMap<>();
+    ConcurrentHashMap<Integer,RaceParticipant> winner = new ConcurrentHashMap<>();
+    
+    public ClickRaceServicesStub(){
+        racesData.put(25, new ConcurrentSkipListSet<>());        
+    }
+    
+    @Override
+    public void registerPlayerToRace(int racenum, RaceParticipant rp) throws ServicesException{
+        if (!racesData.containsKey(racenum)){
+            throw new ServicesException("Race "+racenum+" not registered in the server.");
+        }
+        else{
+            if (racesData.get(racenum).contains(rp)){
+                throw new ServicesException("Racer "+rp.getNumber()+" already registered in race "+racenum);
+            }
+            else{
+                racesData.get(racenum).add(rp);
+            }
+            
+        }
+        
+    }
+
+    @Override
+    public Set<RaceParticipant> getRegisteredPlayers(int racenum) throws ServicesException {
+        return racesData.get(racenum);
+    }
+
+    @Override
+    public void registerWinner(int racenum, RaceParticipant rp) throws ServicesException {
+        if (winner.get(racenum) != null ){
+            throw new ServicesException("Ya hay winner");
+        }
+        winner.put(racenum,rp);
+
+    }
+
+    @Override
+    public int getWinner(int racenum) throws ServicesException {
+        System.out.println(winner);
+        int win = winner.get(racenum).getNumber();
+        return win;
+    }
+
+
+    @Override
+    public void removePlayerFromRace(int racenum, RaceParticipant rp) throws ServicesException {
+        if (!racesData.containsKey(racenum)){
+            throw new ServicesException("Race "+racenum+" not registered in the server.");
+        }
+        else{
+            if (!racesData.get(racenum).contains(rp)){
+                throw new ServicesException("Racer "+rp.getNumber()+" not registered in race "+racenum);
+            }
+            else{
+                racesData.get(racenum).remove(rp);
+            }            
+        }
+    }
+
+
+
+    
+}
---
+++ src/main/resources/static/app.js
@@ -0,0 +1,230 @@
+
+
+//local player data
+var mycar=undefined;
+var mycarxpos=10;
+var mycarypos=10;
+
+
+//competitors data
+var loadedCars=[];
+var carsCurrentXPositions=[];
+var carsCurrentYPositions=[];
+
+let canMove = false;
+
+
+var stompClient = null;
+
+movex = function(){
+    if(canMove == true) {
+        mycarxpos+=10;
+        paintCars();
+        stompClient.send("/topic/car"+mycar.number, {}, JSON.stringify({car:mycar.number,xpos:mycarxpos}));
+        if(mycarxpos > 20){
+            registerWinner()
+            getWinner()
+
+        }
+    }
+    else{
+        alert("Aun no se han registrado 5 jugadores")
+    }
+
+};
+
+init = function(){
+    connectAndSubscribeToCompetitors();
+    $('#manyPlayers').html("El juego arranca cuando haya 5 jugadores")
+    $('#movebutton').html("Aun no se mueve xd")
+}
+
+changeMove = function (){
+    canMove = true;
+}
+
+
+
+
+howManyCars = function (){
+    $.get("races/25/participants",
+        function (data) {
+            loadedCars = data
+            console.log(data.length)
+            if (data.length == 5){
+                loadCompetitorsFromServer()
+                stompClient.send("/topic/canMove",{},true)
+                $('#manyPlayers').html("Ya hay 5 jugadores")
+                $('#movebutton').html("Move My CAR!!!")
+            }
+        })
+}
+
+
+registerWinner = function (){
+    $.ajax({
+        url:"races/25/winner",
+        type:'PUT',
+        data: JSON.stringify(mycar),
+        contentType: "application/json"
+    }).then(
+        function (){
+            alert("winner")
+        },
+        function (err){
+            alert("Ya hay winner")
+        }
+    )
+}
+
+getWinner = function () {
+
+    $.get("races/25/winner",
+        function (data){
+
+        stompClient.send('/topic/winner',{},data)
+
+        })
+}
+
+
+initAndRegisterInServer = function(){
+    mycar={number:$("#playerid").val()};
+    mycarxpos=10;
+    mycarypos=10;
+    
+    $.ajax({
+        url: "races/25/participants",
+        type: 'PUT',
+        data: JSON.stringify(mycar),
+        contentType: "application/json"
+    }).then(
+            function(){                
+                alert("Competitor registered successfully!");
+                paintCars();
+                howManyCars();
+                stompClient.send("/topic/sala", {}, 1)
+            },
+            function(err){
+                alert("err:"+err.responseText);
+            }
+    );
+
+
+};
+
+loadCompetitorsFromServer = function () {
+    if (mycar == undefined) {
+        alert('Register your car first!');
+    } else {
+        $.get("races/25/participants",
+                function (data) {
+                    loadedCars = data;
+                    var carCount = 1;
+                    //alert("Competitors loaded!");
+                    loadedCars.forEach(
+                            function (car) {
+                                if (car.number != mycar.number) {
+                                    carsCurrentXPositions[car.number] = 10;
+                                    carsCurrentYPositions[car.number] = 40 * carCount;
+                                    carCount++;
+                                }
+                            }
+                    );
+                    paintCars();
+                    connectAndSubscribeToCompetitors();
+                }
+        );
+    }
+
+};
+
+setWinner = function (data){
+    $('#winner').html(data)
+}
+
+function connectAndSubscribeToCompetitors() {
+    var socket = new SockJS('/stompendpoint');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        console.log('Connected: ' + frame);
+
+        loadedCars.forEach(
+
+                function (car) {
+                    //don't load my own car
+                    if (car.number!=mycar.number){
+                        stompClient.subscribe('/topic/car'+car.number, function (data) {
+                            msgdata=JSON.parse(data.body);
+                            carsCurrentXPositions[msgdata.car]=msgdata.xpos;
+                            paintCars();
+                        });
+                    }
+                }
+        );
+
+        stompClient.subscribe("/topic/sala",function (data){
+            howManyCars();
+        })
+        stompClient.subscribe("/topic/canMove",function (data){
+             changeMove();
+        })
+        stompClient.subscribe("/topic/winner",function (data) {
+            console.log("en el topic", data.body)
+            setWinner(data.body);
+
+        })
+
+
+        
+    });
+}
+
+function disconnect() {
+    if (stompClient != null) {
+        stompClient.disconnect();
+    }
+    setConnected(false);
+    console.log("Disconnected");
+}
+
+paintCars=function(){
+    canvas=$("#cnv")[0];
+    ctx=canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    //paint my car
+    paint(mycar,mycarxpos,mycarypos,ctx);
+    
+    //paint competitors cars
+    loadedCars.forEach(
+            function(car){
+                paint(car,carsCurrentXPositions[car.number],carsCurrentYPositions[car.number],ctx);
+            }
+    );
+};
+
+
+paint=function(car,xposition,yposition,ctx){
+    
+    var img = new Image;
+    img.src = "img/car.png";
+    img.onload = function(){        
+        ctx.drawImage(img,xposition,yposition); 
+        ctx.fillStyle = "white";
+        ctx.font = "bold 16px Arial";
+        ctx.fillText(""+car.number, xposition+(img.naturalWidth/2), yposition+(img.naturalHeight/2));        
+    };    
+};
+
+
+
+$(document).ready(
+        
+        function () {
+            console.info('loading script!...');
+            $(".controls").prop('disabled', false);    
+            $("#racenum").prop('disabled', true);    
+        }
+);
+
